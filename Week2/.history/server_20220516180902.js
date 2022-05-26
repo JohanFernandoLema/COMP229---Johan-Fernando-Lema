48538d676c4ec29a16c2631a36c75af5b275445f
johan <johan.13fernando@gmail.com>
2022-05-26 11:31:56 -0400

    initial commit

---
+++ Week2/.history/server_20220516180902.js
@@ -0,0 +1,36 @@
+const http = require('http'); // CommonJS Module pattern (CJS)
+const fs = require('fs');
+const mime = required('mime-types');
+
+let lookup = mime.lookup; //alias for mime.lookup
+
+const port = 3000;
+
+const server = http.createServer(function(req, res)   
+{
+    let path = req.url; //alias for req.url
+    if(path == "/" || path == "/home"){
+        path = "/index.html";
+    }
+
+    let mime_type = lookup(path.substring(1));
+    console.log(`MIME TYPE: ${mime_type}`)
+
+    fs.readFile(__dirname + req.url, function(err, data){
+        if(err){
+            res.writeHead(404); //status - file not found
+            return res.end("ERROR: 404 - File not found "); //output the error message to the page
+        }
+        //no error
+        res.setHeader("X-Content-Type-Options","nosniff") //security guard
+        res.writeHead(200, {'Content-Type': mime_type}); //status - all ok 
+        return res.end(data); //output the file that was read to the page
+
+    })
+
+});
+
+server.listen(port, function() 
+{
+    console.log(`Server running at Port: ${port}`);
+});
